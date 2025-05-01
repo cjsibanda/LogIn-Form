b95496f4d29534151e5bbe02abe197687a5283b1
Anesu C.J Sibanda <87155971+cjsibanda@users.noreply.github.com>
2025-05-01 12:44:31 -0400

    Update app.component.spec.ts

--- login-page/src/app/app.component.spec.ts
+++ login-page/src/app/app.component.spec.ts
@@ -26,6 +26,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('login-page');
   });
 
+  //Should render title
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
